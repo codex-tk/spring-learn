0a2e3f2081cd10935454b8bdb5704edc25e734c2
tk <aoziczero@gmail.com>
2017-11-11 00:35:49 +0900

    Reflection & Proxy test base

---
+++ src/test/java/org/codex/ProxyTest.java
@@ -0,0 +1,50 @@
+package org.codex;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.*;
+
+public class ProxyTest {
+
+    public interface Hello{
+        String sayHello( String name );
+        String sayHi( String name  );
+        String sayThankYou( String name );
+    }
+
+    public static class HelloTarget implements Hello {
+        public String sayHello(String name) {  return "Hello " + name;  }
+        public String sayHi(String name) { return "Hi " + name; }
+        public String sayThankYou(String name) { return "ThankYou " + name; }
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class UpperCaseHandler implements InvocationHandler {
+        Hello hello;
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            String ret = (String)method.invoke(hello,args);
+            return ret.toUpperCase();
+        }
+    }
+    @Test
+    public void dynamicProxy() {
+        Hello helloProxy = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader()
+                , new Class[] { Hello.class }
+                , new UpperCaseHandler( new HelloTarget() ));
+
+        assertEquals( helloProxy.sayHello("tk") , "HELLO TK");
+    }
+}
---
+++ src/test/java/org/codex/ReflectionTest.java
@@ -0,0 +1,24 @@
+package org.codex;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.*;
+
+public class ReflectionTest {
+    @Test
+    public void invokeMethod() throws Exception{
+        String name = "Spring";
+        assertThat( name.length() , is(6) );
+        Method lengthMethod = String.class.getMethod("length");
+        assertThat( lengthMethod.invoke(name) , is(6) );
+
+        assertThat( name.charAt(0) , is('S'));
+        Method charAtMethod = String.class.getMethod("charAt", int.class);
+        assertThat( charAtMethod.invoke(name , 0) , is('S'));
+
+    }
+}
